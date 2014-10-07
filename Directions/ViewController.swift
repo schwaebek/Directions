f48ffbfe229e04ed0d8aa325f945818fe8753f95
Katlyn Schwaebe <schwaebek@gmail.com>
2014-10-07 12:17:37 -0400

    map directions

--- Directions/ViewController.swift
+++ Directions/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+        routingMap.showsUserLocation = true
+        routingMap.delegate = self
+        
+    }
+    @IBOutlet weak var routingMap: MKMapView!
+    @IBOutlet weak var addressField: UITextField!
+
+    @IBAction func route(sender: AnyObject) {
+        let address = addressField.text
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.geocodeAddressString(address, completionHandler: { (placemarks, error) -> Void in
+            if placemarks.count > 0 {
+                self.findRoute(placemarks[0] as CLPlacemark)
+            }
+        })
+    }
+    func findRoute(placemark: CLPlacemark) {
+        
+        let mkPlacemark = MKPlacemark(placemark: placemark)
+        
+        let start = MKMapItem.mapItemForCurrentLocation()
+        let end = MKMapItem(placemark: mkPlacemark)
+        
+    
+     //   let coordStart = routingMap.userLocation
+       // let coordEnd = placemark.location.coordinate
+        
+        var directionsRequest = MKDirectionsRequest()
+        
+        directionsRequest.setSource(start)
+        directionsRequest.setDestination(end)
+        
+        var directions = MKDirections(request: directionsRequest)
+        directions.calculateDirectionsWithCompletionHandler { (directionsResponse, error) -> Void in
+            if directionsResponse.routes.count > 0 {
+                var route = directionsResponse.routes[0] as MKRoute
+                self.routingMap.addOverlay(route.polyline)
+                
+            }
+        }
+        
+    }
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        var routeRenderer = MKPolylineRenderer(overlay: overlay)
+        routeRenderer.strokeColor = UIColor.purpleColor()
+        routeRenderer.lineWidth = 2
+        return routeRenderer
     }
 
     override func didReceiveMemoryWarning() {
